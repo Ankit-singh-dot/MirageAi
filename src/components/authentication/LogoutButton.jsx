3aba5e19752b34f579d021272fbcc4b55ad98ff1
Ankit <ankitrajdihara123@gmail.com>
2025-11-20 23:55:28 +0530

    momentum_jsx

--- src/components/authentication/LogoutButton.jsx
+++ src/components/authentication/LogoutButton.jsx
@@ -1,10 +1,10 @@
 "use client";
 import React from "react";
-
+import { logout } from "@/app/actions/auth-actions";
 const LogoutButton = () => {
-  // const handleLogout = async () => {
-  //   await 
-  // };
+  const handleLogout = async () => {
+    await logout();
+  };
   return <div></div>;
 };
 
